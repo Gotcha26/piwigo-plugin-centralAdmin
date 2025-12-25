e8d15f772f1c3f93696136c59c55d9e8ee3003a5
Gotcha26 <spambiengentil@gmail.com>
2025-12-25 13:20:08 +0100

    Préparation à publication

--- main.inc.php
+++ main.inc.php
@@ -4,7 +4,7 @@ Plugin Name: Central Admin CSS
 Description: Centrage de toute l'administration sur une colonne maximum de 1600px.
              Tient compte de la couleur (clair / obscur).
              Injecte des feuilles CSS personnalisées uniquement.
-Version: 1.4
+Version: 1.0
 Author: Gotcha
 */
 
